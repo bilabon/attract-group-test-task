0251f384111da9b492d1a6647a01934f3880c8fa
Dmytro Bazas <babyx64@gmail.com>
2016-03-10 21:13:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+Test task for the Attract Group
+======
+
+Agreement
+=========
+
+- The `(.env)$` identifier of command line is indicates that there must be active virtual environment.
+
+- In this manual path to the project, for example: `/full/path/to/the/attract-group-test-task/` - it will be designated as `$BASE_DIR`.
+
+
+Attention
+=========
+
+- Python support: `2.7`
+
+-------
+
+Quick start guide
+=================
+
+Clone
+-----
+
+
+    $ git clone git@github.com:bilabon/attract-group-test-task.git
+    $ cd attract-group-test-task/
+
+
+Install virtualenv
+------------------
+
+
+    $ virtualenv --no-site-packages -p/usr/bin/python2.7 .env
+    $ source .env/bin/activate
+    (.env)$
+
+
+Install packages
+----------------
+
+
+    (.env)$ pip install -r requirements.txt
---
+++ basic/project_settings.py
@@ -0,0 +1,68 @@
+"""
+Django settings for test task.
+"""
+
+import os
+import sys
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+ALLOWED_HOSTS = ['127.0.0.1']
+SITE_ID = 1
+
+
+sys.path.append(os.path.join(BASE_DIR, 'apps'), )
+
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'web', 'media')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'web', 'static')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'logs', 'email')
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'django_file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'django.log'),
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['django_file'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+    }
+}
